58543bcd4236d42d69d11ecdd4cd912ba7bbe2f0
Adey4k <150963491+Adey4k@users.noreply.github.com>
2025-03-09 15:03:10 +0200

    Update TestResult.java

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,12 +1,37 @@
 package test;
 
-import domain.Exercise;
+import java.util.Scanner;
+import static domain.Exercise.Calculate;
 
+
+/**
+ * Клас відповідає за взаємодію з користувачем
+ */
 public class TestResult {
-
     public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        Scanner scanner = new Scanner(System.in);
+        boolean i = true;
+        
+        while (i == true) {
+            System.out.print("Enter number, or 'q' to quit: ");
+            
+            if (scanner.hasNextInt()) {
+                int number = scanner.nextInt();
+                if (Calculate(number)) {
+                    System.out.println(number + " is an Armstrongs number");
+                } else {
+                    System.out.println(number + " is NOT an Armstrongs number");
+                }
+            } else {
+                String input = scanner.next();
+                if (input.equalsIgnoreCase("q")) {
+                    i = false;
+                } else {
+                    System.out.println("Unknown command");
+                }
+            }
+        }
+        
+        scanner.close();
     }
 }
